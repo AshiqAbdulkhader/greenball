4d27acf9c83ee4f6a579cc2764c7b2abf7b7efc2
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2020-01-30 20:29:45 +0530

    Arduino Code

---
+++ ArduinoCode.ino
@@ -0,0 +1,27 @@
+int data;
+void setup() {
+  Serial.begin(9600);
+  pinMode(12,OUTPUT); //pin for red LED
+  pinMode(8,OUTPUT); //pin for blue LED
+  
+
+}
+
+void loop() {
+  while (Serial.available())  //checks for serial data
+  {
+    data = Serial.read();    //read serial data
+    if(data=='1')
+    {
+      digitalWrite(8,HIGH);    //if serial data is 1, green LED glows
+      digitalWrite(12,LOW);
+    }
+    else if(data=='0')
+    {
+      digitalWrite(12,HIGH);   // //if serial data is 0, red LED glows
+      digitalWrite(8,LOW);
+    }
+    
+  }
+
+}
